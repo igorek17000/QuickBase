9e0fb1b4a33fc5ca8a473e4f937508026981f09f
William C <56387759+williamchnt@users.noreply.github.com>
2022-02-17 12:16:58 +0100

    Creation model APIKey

--- QuickBase-app/accounts/models.py
+++ QuickBase-app/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class APIKey(models.Model):
+    FTX = 'FTX'
+    BINANCE = 'BIN'
+    Exchange_platforme = [
+        (FTX, 'FTX'),
+        (BINANCE, 'Binance'),
+    ]
+    exchange = models.CharField(
+        max_length=3,
+        choices=Exchange_platforme,
+        default=FTX,
+    )
+    apiKey = models.CharField(max_length=50)
+    apiKeySecret = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
