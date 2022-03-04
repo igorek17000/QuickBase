f0cc1d84a448237ff9eb2ae351ffa3188aecb60d
William C <56387759+williamchnt@users.noreply.github.com>
2022-03-04 15:06:08 +0100

    status account

--- QuickBase-app/accounts/views.py
+++ QuickBase-app/accounts/views.py
@@ -43,42 +43,54 @@ def myaccount(request):
         return redirect('/accounts/login')
     else :
         if request.method == "POST":
-            print(request.POST['password3'])
+            try:
+                APIKey.objects.get(user = request.user,exchange = 'FTX')
+                APIFTX = APIKey.objects.filter(user = request.user,exchange = 'FTX')
+                FTXKey = APIFTX[0].apiKey
+                FTXSecret = APIFTX[0].apiKeySecret
+            except APIKey.DoesNotExist:
+                FTXKey = 0
+                FTXSecret = 0
+            try:
+                username= request.user.username
+                email= request.user.email
+            except:
+                username = "nom d'utilisateur"
+                email = "Email"
+
             if check_password(request.POST['password3'], request.user.password):
                 try:
-                    User.objects.filter(username=request.user.username,email=request.user.email).update(username=request.POST['username'],email = request.POST['email'])
                     if request.POST['password1']:
                         if request.POST['password1'] == request.POST['password2']:
-                            print("mdp changed")
+                            User.objects.filter(username=request.user.username,email=request.user.email).update(username=request.POST['username'],email = request.POST['email'])
                             U = User.objects.get(username=request.user.username)
                             U.set_password(request.POST['password1'])
                             U.save()
                         else :
                             print("mdp différents")
-                            return render (request,'accounts/myaccount.html', {'status':'Mots de passe différents!'})
+                            return render (request,'accounts/account.html', {'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email,'error':'Mots de passe différents!','status':1})
                 except:
-                    print(str(0))
+                    return render (request,'accounts/account.html', {'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email,'error':'Impossible de modifier les infos!','status':1})
                 
                 if request.POST['APIKey'] or request.POST['APISecret']:
                     try:
                         APIKey.objects.get(user = request.user)
                         APIKey.objects.get(exchange = 'FTX')
                         APIKey.objects.filter(user=request.user,exchange = 'FTX').update(apiKey=request.POST['APIKey'],apiKeySecret = request.POST['APISecret'])
-                        return render (request,'dashboard/dashboard.html', {'success':'Clé API déjà existante'})
+                        return render (request,'accounts/account.html', {'success':'Clé API modifiée','status':0})
                     except APIKey.DoesNotExist:
                         try:
                             apiKey = APIKey(exchange = 'FTX',apiKey=request.POST['APIKey'],apiKeySecret = request.POST['APISecret'],user=request.user)
                             apiKey.save()
                             #return redirect('overview')
-                            return render (request,'dashboard/dashboard.html',{'success':'Clé API ajouté'})
+                            return render (request,'accounts/account.html',{'success':'Clé API ajouté','status':0})
                         except:
-                            return render (request, 'dashboard/dashboard.html',{"status':'Erreur lors de la clé API"})
+                            return render (request, 'accounts/account.html',{'error':'Erreur lors de la clé API','status':1})
 
                 else:
-                    return render (request,'dashboard/dashboard.html',{'success':'Informations modifées'})
+                    return render (request,'accounts/account.html',{'success':'Informations modifées','status':0})
             else:
-                print('Mot de passe incorrect')
-                return render(request, 'accounts/account.html',{'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email,'error':'Mot de passe incorrect'})
+                return render(request, 'accounts/account.html',{'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email,'error':'Mot de passe incorrect','status':1})
 
         else:
             try:
@@ -97,4 +109,5 @@ def myaccount(request):
                 username = "nom d'utilisateur"
                 email = "Email"
 
-            return render(request, 'accounts/account.html',{'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email})
+            return render(request, 'accounts/account.html',{'FTXKey':FTXKey,'FTXSecret':FTXSecret,'username':username,'email':email,'status':0})
+
